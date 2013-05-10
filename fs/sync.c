e423e9f6b76f53e29aaa2f44ad2bea230775eec2
Ajay katwe <ajay.katwe@gmail.com>
2013-05-10 17:38:07 +0530

    revert dependencies

--- fs/sync.c
+++ fs/sync.c
@@ -21,9 +21,7 @@
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
-#ifdef CONFIG_FSYNC_CONTROL
-extern bool fsynccontrol_fsync_enabled();
-#endif
+
 
 /*
  * Do the filesystem syncing work. For simple filesystems
@@ -177,10 +175,7 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	struct address_space *mapping = file->f_mapping;
 	int err, ret;
 
-#ifdef CONFIG_FSYNC_CONTROL
-        if (!fsynccontrol_fsync_enabled())
-            return 0;
-#endif
+
 
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
@@ -227,10 +222,7 @@ static int do_fsync(unsigned int fd, int datasync)
 	struct file *file;
 	int ret = -EBADF;
 
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
+
 
 	file = fget(fd);
 	if (file) {
@@ -242,10 +234,6 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
 
 	return do_fsync(fd, 0);
 }
@@ -270,9 +258,7 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
  */
 int generic_write_sync(struct file *file, loff_t pos, loff_t count)
 {
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
+
 #endif
 
 	if (!(file->f_flags & O_DSYNC) && !IS_SYNC(file->f_mapping->host))
@@ -338,10 +324,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 	loff_t endbyte;			/* inclusive */
 	int fput_needed;
 	umode_t i_mode;
-#ifdef CONFIG_FSYNC_CONTROL
-	if (!fsynccontrol_fsync_enabled())
-		return 0;
-#endif
+
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
